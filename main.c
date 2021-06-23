9095a7da2ecda442730a448d8c2badd65f0f4b28
CarlosCraveiro <85318248+CarlosCraveiro@users.noreply.github.com>
2021-06-22 21:57:30 -0300

    Mudanças Reunião
    Codigo modificado durtante a reunição.

--- main.c
+++ main.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
-//Configurações de administrador
-#define ADMIN 1
-int main(int argc, char *argv[]) {
-    // Declaração e inicialização do tabuleiro
-    int tabuleiro[4][4];
-    for(int linha = 0; linha < 4; linha++) {
-        for(int coluna = 0; coluna < 4; coluna++) {
-            tabuleiro[linha][coluna] = -1;
-        }
-    }
-    // Contador de rodadas
-    int semi_rodada = 0;
-    // Jogo
-    int houver_vencedor = 0;
-    while(!houver_vencedor) {
-        semi_rodada++;
-        unsigned int peca = 0;
-        scanf("%x", &peca);
-        unsigned int posicao = 0;
-        scanf("%x", &posicao);
-        // Adequação no tabuleiro
-        tabuleiro[posicao / 4][posicao % 4] = peca;
 
-        //Verificação de se há um vencedor
-        int vitoria = 0;
+int verificar_quarto();
+
+int caracteristica_vencedora();
+
+int varre_tabuleiro() {
+    int vitoria = 0;
         //para cada linha
         for(int linha = 0; linha < 4; linha++) {
+            quarto += verificar_quarto(tabuleiro[linha]);
             int quarto = -1;
-            for(int coluna = 0; coluna < 4; coluna++) {
-                if(tabuleiro[linha][coluna] != -1) {
-                    if(quarto == -1) {
+            for (int coluna = 0; coluna < 4; coluna++) {
+                if (tabuleiro[linha][coluna] != -1) {
+                    if (quarto == -1) {
                        quarto = tabuleiro[linha][coluna];
                     }
                     else {
@@ -42,14 +25,14 @@ int main(int argc, char *argv[]) {
             }
         }
         //para cada coluna
-        for(int coluna = 0; coluna < 4; coluna++) {
+        for (int coluna = 0; coluna < 4; coluna++) {
             int quarto = 0;
-            for(int linha = 0; linha < 4; linha++) {
+            for (int linha = 0; linha < 4; linha++) {
                 if(tabuleiro[linha][coluna] != -1) {
-                    if(quarto == -1) {
+                    if (quarto == -1) {
                        quarto = tabuleiro[linha][coluna];
                     }
-                    else{
+                    else {
                         quarto = ~(quarto ^ tabuleiro[linha][coluna]);
                     }
                 }
@@ -89,19 +72,43 @@ int main(int argc, char *argv[]) {
         if(vitoria > 0) {
             houver_vencedor = 1;
         }
+}
+
+
+int main(int argc, char *argv[]) {
+    // Declaração e inicialização do tabuleiro
+    int tabuleiro[4][4];
+    for(int linha = 0; linha < 4; linha++) {
+        for(int coluna = 0; coluna < 4; coluna++) {
+            tabuleiro[linha][coluna] = -1;
+        }
+    }
+    // Contador de rodadas
+    int semi_rodada = 0;
+    int sequencia = {-1, -1, -1, -1}
+    // Jogo
+    int houver_vencedor = 0;
+    while(!houver_vencedor) {
+        semi_rodada++;
+        unsigned int peca = 0;
+        scanf("%x", &peca);
+        unsigned int posicao = 0;
+        scanf("%x", &posicao);
+        // Adequação no tabuleiro
+        tabuleiro[posicao / 4][posicao % 4] = peca;
+
+        //Verificação de se há um vencedor
+        sequencia = varre_tabuleiro(&houver_vencedor, tabuleiro);
     }
     // Saída do Tabuleiro
     for(int linha = 0; linha < 4; linha++) {
         for(int coluna = 0; coluna < 4; coluna++) {
             char vazio = '\0';
-            vazio = (tabuleiro[linha][coluna] != -1)? '\0':'-';
+            vazio = (tabuleiro[linha][coluna] != -1)? '\0': '-';
             (vazio)? printf("%c", vazio):printf("%x", tabuleiro[linha][coluna]);
         }
         printf("\n");
     }
-    // Exibição de vencedor caso o modo de ADMIN esteja ligado
-    if(ADMIN == 1) {
-        printf("O %s VENCEU!!", (semi_rodada % 2 == 0)? "Jogador 1":"Jogador 2");
-    }
-//TESTE DE ALTERAÇÃO
+
+    printf("%s", (semi_rodada % 2 == 0) ? "1" : "2");
 }
