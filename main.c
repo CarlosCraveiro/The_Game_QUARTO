c4f029e4f98c20b0d70cef2c9983f830a1820ef4
CarlosCraveiro <85318248+CarlosCraveiro@users.noreply.github.com>
2021-06-30 17:43:36 -0300

    Implementação das funções na main
    Implementei as funções discutidas na reunião na main.
    
    Vou fazer mais um commit daqui a pouco, só para organizar a função varre tabuleiro e conseguir compilar o codigo na minha máquina

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int verificar_quarto();
+int verificar_quarto(int quad);
 
-int caracteristica_vencedora();
+int caracteristica_vencedora(int quadra);
 
-int varre_tabuleiro() {
+int varre_tabuleiro(int *venceu, int *coordenadas, int tabuleiro) {
     int vitoria = 0;
+    int quarto;
         //para cada linha
         for(int linha = 0; linha < 4; linha++) {
             quarto += verificar_quarto(tabuleiro[linha]);
@@ -70,7 +71,7 @@ int varre_tabuleiro() {
             vitoria++;
         }
         if(vitoria > 0) {
-            houver_vencedor = 1;
+            venceu = 1;
         }
 }
 
@@ -85,30 +86,48 @@ int main(int argc, char *argv[]) {
     }
     // Contador de rodadas
     int semi_rodada = 0;
-    int sequencia = {-1, -1, -1, -1}
+    // Parametros do Jogo
+    int pecas_sequencia[3];
+    int coord_sequencia[3];
+    char caracteristica[10];
     // Jogo
-    int houver_vencedor = 0;
-    while(!houver_vencedor) {
+    int *houver_vencedor = 0;
+    while(!houver_vencedor && semi_rodada < 16) {
         semi_rodada++;
-        unsigned int peca = 0;
-        scanf("%x", &peca);
-        unsigned int posicao = 0;
-        scanf("%x", &posicao);
+        unsigned int peca;
+        scanf("%X", &peca);
+        unsigned int posicao;
+        scanf("%X", &posicao);
         // Adequação no tabuleiro
         tabuleiro[posicao / 4][posicao % 4] = peca;
 
-        //Verificação de se há um vencedor
-        sequencia = varre_tabuleiro(&houver_vencedor, tabuleiro);
+        // Verificação de se há um vencedor
+        pecas_sequencia = varre_tabuleiro(&houver_vencedor, &coord_sequencia, tabuleiro);
+
+        if(houver_vencedor) {
+            // Identifica a caracteristica
+            caracteristica = caracteristica_vencedora(pecas_sequencia);
+        }
     }
     // Saída do Tabuleiro
     for(int linha = 0; linha < 4; linha++) {
         for(int coluna = 0; coluna < 4; coluna++) {
             char vazio = '\0';
             vazio = (tabuleiro[linha][coluna] != -1)? '\0': '-';
-            (vazio)? printf("%c", vazio):printf("%x", tabuleiro[linha][coluna]);
+            (vazio)? printf("%c", vazio) : printf("%X", tabuleiro[linha][coluna]);
         }
         printf("\n");
     }
-
-    printf("%s", (semi_rodada % 2 == 0) ? "1" : "2");
+    // Saída dos outros atributos
+    if(*houver_vencedor) {
+        printf("%s\n", (semi_rodada % 2 == 0) ? "1" : "2");
+        for(int i = 0; i < 4; i++) {
+            printf("%X", coord_sequencia[i]);
+        }
+        printf("\n");
+        printf("%s", caracteristica);
+    }
+    else {
+        printf("0");
+    }
 }
